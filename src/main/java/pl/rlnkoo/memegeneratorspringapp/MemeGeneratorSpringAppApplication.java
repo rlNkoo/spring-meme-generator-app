f3ee6c77903523e97d7c111e1dacde4958acef74
rlNkoo <rolakartur96@gmail.com>
2024-04-22 19:06:28 +0200

    First commit

---
+++ src/main/java/pl/rlnkoo/memegeneratorspringapp/MemeGeneratorSpringAppApplication.java
@@ -0,0 +1,13 @@
+package pl.rlnkoo.memegeneratorspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeGeneratorSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeGeneratorSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rlnkoo/memegeneratorspringapp/MemeGeneratorSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rlnkoo.memegeneratorspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeGeneratorSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
