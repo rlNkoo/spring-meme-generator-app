879e841283cc8310b136bc8159ebffce8b91bdfb
rlNkoo <rolakartur96@gmail.com>
2024-04-28 14:10:52 +0200

    MemeItemController updated

--- src/main/java/pl/rlnkoo/memegeneratorspringapp/controllers/MemeItemController.java
+++ src/main/java/pl/rlnkoo/memegeneratorspringapp/controllers/MemeItemController.java
@@ -1,2 +1,21 @@
-package pl.rlnkoo.memegeneratorspringapp.controllers;public class MemeItemController {
+package pl.rlnkoo.memegeneratorspringapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.rlnkoo.memegeneratorspringapp.models.MemeItem;
+import pl.rlnkoo.memegeneratorspringapp.services.MemeItemService;
+
+@RestController
+@RequestMapping("/api/meme-items")
+public class MemeItemController {
+
+    @Autowired
+    private MemeItemService memeItemService;
+
+    @GetMapping("")
+    public Iterable<MemeItem> getMemeItems() {
+        return memeItemService.getAll();
+    }
 }
